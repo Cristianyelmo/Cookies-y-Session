4aa58381d14e4bb152b076a8593a96ff9cfc8601
Cristianyelmo <cristianama2019@gmail.com>
2023-03-20 16:40:26 -0300

    sweetalert interactivo

--- public/javascripts/sweetAlert.js
+++ public/javascripts/sweetAlert.js
@@ -1,39 +1,22 @@
 const formcreate = document.getElementById("form-colores")
 
-
-
-
-
-
-
-
-
-
-
-
-    
 formcreate.addEventListener("submit",function(event){
 event.preventDefault();
+const nameUser = event.target.dataset.user
 
 Swal.fire({
     position: 'middle',
     imageUrl: 'https://media.tenor.com/ZRmKGlWN4XEAAAAC/sakuragi-hanamichi.gif',
   imageHeight: 300,
   imageWidth: 300,
-    title: 'Listou!',
+    title: `Bienvenido ${nameUser}`,
     showConfirmButton: false,
-  timer: 4300 
-  })
-
-
-  formcreate.submit()
-
-
-
-
-
-
-
+  timer: 2000 ,
+  background: 'linear-gradient(270deg, rgba(255, 255, 255, 0.07) 0%, rgba(2, 152, 227, 0.7) 33.33%, rgba(1, 58, 87, 0.7) 66.67%, rgba(0, 0, 0, 0.7) 100%)',
+  color:'white'
+  }).then(() => 
+    formcreate.submit() 
+    )
 
 
 
