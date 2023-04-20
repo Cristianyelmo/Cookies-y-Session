315e5d70df1155bcfae1daf4d64bc8328c602afe
Cristianyelmo <cristianama2019@gmail.com>
2023-04-20 07:17:03 -0300

    sonido y 2 tipos de sweetalert

--- public/javascripts/sonido.js
+++ public/javascripts/sonido.js
@@ -1,16 +1,41 @@
-const sonidoxd = document.getElementById('sonidoxd')
+ const sonidoxd = document.getElementById('sonidoxd')
+const xd = document.getElementById('xd') 
+
+
 
 let audio = new Audio
 
 
+
+
 const playTune = (key) =>{
     
-    
-    audio.src = `/sound/MagicCartoon CTE01_93.3_preview.mp3`
+   
+    if(sonidoxd){
 
+        audio.src = `/sound/MagicCartoon CTE01_93.3_preview.mp3`
+
+        audio.play()
+    
+    }
+
+     if(xd){
+        audio.src = `/sound/Efecto de sonido - Error.mp3`
+
+        audio.play()
+    }
+ 
 
     
-    audio.play()
+    
 }
 
-sonidoxd.addEventListener('click',()=>playTune())
+
+
+(sonidoxd||xd).addEventListener('click',()=>playTune())
+
+
+
+
+
+
--- public/javascripts/sweetAlert.js
+++ public/javascripts/sweetAlert.js
@@ -4,21 +4,71 @@ formcreate.addEventListener("submit",function(event){
 event.preventDefault();
 const nameUser = event.target.dataset.user
 
-Swal.fire({
+
+const nameerror = event.target.dataset.error
+
+
+
+if(nameerror){
+
+  Swal.fire({
     position: 'middle',
-    imageUrl: 'https://media.tenor.com/ZRmKGlWN4XEAAAAC/sakuragi-hanamichi.gif',
+    imageUrl: 'https://i.pinimg.com/originals/a3/f6/b6/a3f6b6cce681137729e24bdeeed68b77.gif',
   imageHeight: 300,
   imageWidth: 300,
-    title: `Bienvenido ${nameUser}`,
+    title: `Incorrecto`,
     showConfirmButton: false,
-  timer: 2000 ,
-  background: 'linear-gradient(270deg, rgba(255, 255, 255, 0.07) 0%, rgba(2, 152, 227, 0.7) 33.33%, rgba(1, 58, 87, 0.7) 66.67%, rgba(0, 0, 0, 0.7) 100%)',
+  timer: 3500 ,
+  background: 'red',
   color:'white'
   }).then(() => 
+  
+  
+
+
+
+
+
     formcreate.submit() 
     )
 
 
 
+
+
+
+    
+    
+    
+  } else{
+
+
+    Swal.fire({
+      position: 'middle',
+      imageUrl: 'https://media.tenor.com/ZRmKGlWN4XEAAAAC/sakuragi-hanamichi.gif',
+    imageHeight: 300,
+    imageWidth: 300,
+      title: `Bienvenido ${nameUser}`,
+      showConfirmButton: false,
+    timer: 3500 ,
+    background: 'linear-gradient(270deg, rgba(255, 255, 255, 0.07) 0%, rgba(2, 152, 227, 0.7) 33.33%, rgba(1, 58, 87, 0.7) 66.67%, rgba(0, 0, 0, 0.7) 100%)',
+    color:'white'
+    }).then(() => 
+    
+    
+  
+  
+  
+  
+  
+      formcreate.submit() 
+      )
+    
+  }
+
+
+
+
 })
+
 
